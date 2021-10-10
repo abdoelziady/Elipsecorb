d209a83085b54e337612c9f2caa87a46078a9320
abdoelziady <52676126+abdoelziady@users.noreply.github.com>
2021-10-10 12:42:27 +0200

    add all files

---
+++ js/soo.js
@@ -0,0 +1,29 @@
+$(document).ready(function()
+                 {
+$(".work ul li.ll").click(function()
+  {
+        $(".work .mdimg").animate({opacity:.1});
+    $(".work .mdimg.jj").animate({opacity:1});
+});
+    
+    $(".work ul li.kk").click(function()
+  {
+        $(".work .mdimg").animate({opacity:.1});
+    $(".work .mdimg.mm").animate({opacity:1});
+});
+     $(".work ul li.all").click(function()
+  {
+        $(".work .mdimg").animate({opacity:1});
+});
+     $(".work ul li.qq").click(function()
+  {
+        $(".work .mdimg").animate({opacity:.1});
+    $(".work .mdimg.ww").animate({opacity:1});
+});
+     $(".work ul li.ee").click(function()
+  {
+        $(".work .mdimg").animate({opacity:.1});
+    $(".work .mdimg.rr").animate({opacity:1});
+});
+    
+});
